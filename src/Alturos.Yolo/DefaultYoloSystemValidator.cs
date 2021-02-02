b8497e65b09abe278be7e5b06962b38c29a48960
Gábor Zavarkó <gabor.zavarko@horizonte.as>
2021-02-03 00:17:18 +0100

    better cuda detection

--- src/Alturos.Yolo/DefaultYoloSystemValidator.cs
+++ src/Alturos.Yolo/DefaultYoloSystemValidator.cs
@@ -4,13 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Alturos.Yolo
 {
     public class DefaultYoloSystemValidator : IYoloSystemValidator
     {
-        public static Version CudaVersion = new Version(10, 2);
+        public static byte[] CudnnPattern = Encoding.ASCII.GetBytes("cudnn64_*.dll\0");
+        public static byte[] CudartPattern = Encoding.ASCII.GetBytes("cudart64_*.dll\0");
 
         public SystemValidationReport Validate()
         {
@@ -34,57 +36,25 @@ namespace Alturos.Yolo
 #endif
 
             const string yoloDllFileName = "yolo_cpp_dll_gpu.dll";
-            string cudnnFileName = "cudnn64_7.dll";
+            int cudnnVersion = 7;
+            int cudartVersion = 102; // 10.2
+
             if (File.Exists(yoloDllFileName))
             {
                 report.YoloGpuDll = true;
                 var data = File.ReadAllBytes(yoloDllFileName);
-                for (int i = 0; i < data.Length - 24; i++)
+                if (FindVersion(data, CudnnPattern, out int version))
                 {
-                    if (data[i] != 'c') continue;
-                    if (data[i + 1] != 'u') continue;
-                    if (data[i + 2] != 'd') continue;
-                    if (data[i + 3] != 'n') continue;
-                    if (data[i + 4] != 'n') continue;
-                    if (data[i + 5] != '6') continue;
-                    if (data[i + 6] != '4') continue;
-                    if (data[i + 7] != '_') continue;
+                    cudnnVersion = version;
+                }
 
-                    int j = i + 8;
-                    int version = 0;
-                    int digits = 0;
-                    bool ok = true;
-                    while (true)
-                    {
-                        int ch = data[j];
-                        if (ch == '.' || digits > 9)
-                        {
-                            break;
-                        }
-
-                        if (ch < '0' || ch > '9')
-                        {
-                            ok = false;
-                            break;
-                        }
-
-                        version *= 10;
-                        version += ch - '0';
-                        digits++;
-                        j++;
-                    }
-
-                    if (!ok) continue;
-                    if (data[j + 1] != 'd') continue;
-                    if (data[j + 2] != 'l') continue;
-                    if (data[j + 3] != 'l') continue;
-                    if (data[j + 4] != 0) continue;
-
-                    cudnnFileName = $"cudnn64_{version}.dll";
-                    break;
+                if (FindVersion(data, CudartPattern, out version))
+                {
+                    cudartVersion = version;
                 }
             }
 
+            string cudnnFileName = $"cudnn64_{cudnnVersion}.dll";
             if (File.Exists(cudnnFileName))
             {
                 report.CudnnExists = true;
@@ -96,12 +66,89 @@ namespace Alturos.Yolo
                 report.CudaExists = true;
             }
 
-            if (environmentVariables.Contains($"CUDA_PATH_V{CudaVersion.Major}_{CudaVersion.Minor}"))
+            int cudaVersionMajor = cudartVersion / 10;
+            int cudaVersionMinor = cudartVersion % 10;
+            if (environmentVariables.Contains($"CUDA_PATH_V{cudaVersionMajor}_{cudaVersionMinor}"))
             {
                 report.CudaExists = true;
             }
 
             return report;
+        }
+
+        private bool FindVersion(byte[] data, byte[] pattern, out int version)
+        {
+            int starIdx = -1;
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (pattern[i] == '*')
+                {
+                    starIdx = i;
+                    break;
+                }
+            }
+
+            if (starIdx == -1)
+            {
+                version = 0;
+                return false;
+            }
+
+            for (int i = 0; i < data.Length - 24; i++)
+            {
+                bool ok = true;
+                for (int k = 0; k < starIdx; k++)
+                {
+                    if (data[i + k] != pattern[k])
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if (!ok) continue;
+
+                int j = i + starIdx;
+                version = 0;
+                int digits = 0;
+                while (true)
+                {
+                    int ch = data[j];
+                    if (ch == '.' || digits > 9)
+                    {
+                        break;
+                    }
+
+                    if (ch < '0' || ch > '9')
+                    {
+                        ok = false;
+                        break;
+                    }
+
+                    version *= 10;
+                    version += ch - '0';
+                    digits++;
+                    j++;
+                }
+
+                if (!ok) continue;
+
+                for (int k = starIdx + 1; k < pattern.Length; k++)
+                {
+                    if (data[j + k - starIdx - 1] != pattern[k])
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if (!ok) continue;
+
+                return true;
+            }
+
+            version = 0;
+            return false;
         }
 
         private bool IsMicrosoftVisualCPlusPlus2017Available()
